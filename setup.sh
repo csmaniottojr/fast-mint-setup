1ba11a88522f7410addaa10bad216c539d92ba99
Cesar Smaniotto Júnior <cesarsjb@gmail.com>
2024-02-15 10:36:04 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,20 +17,10 @@ sudo apt install zsh -y
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# pip
-sudo apt install python3-pip
-pip install --upgrade pip
-
 # pyenv
-sudo apt-get update; sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
 curl https://pyenv.run | bash
 
-# libpq-dev (dependencia do psycopg)
-sudo apt-get install libpq-dev -y
-
-# python-dev (dependencia do psycopg)
-sudo apt-get install python-dev python3-dev -y
+# adicionar pyenv no ~/.zshrc
 
 # snap (precisa reiniciar após a instalação)
 sudo rm /etc/apt/preferences.d/nosnap.pref
@@ -52,18 +42,6 @@ sudo snap install dbeaver-ce
 # sublime
 sudo snap install sublime-text --classic
 # instalar package control https://packagecontrol.io/installation
-
-# insomnia
-sudo snap install insomnia
-
-# mongodb client
-sudo snap install robo3t-snap
-
-# vpnc
-sudo apt install vpnc
-
-# remmina (rdp client)
-sudo snap install remmina
 
 # flameshot (a better screenshot software)
 sudo snap install flameshot
